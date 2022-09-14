e3232457123d58f486f9e9203cd382c9b130adfa
Evanflow89 <ivanbaragone@gmail.com>
2022-09-14 11:25:20 +0200

    Add Vue Js

---
+++ resources/js/front.js
@@ -0,0 +1,9 @@
+window.Vue = require('vue');
+
+import App from './views/App';
+
+const app = new Vue({
+    el: '#app',
+    render: h => h(App)
+});
+
---
+++ resources/js/views/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>VueJs</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+</style>
