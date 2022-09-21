6a20bdafe254b5e8859c3d81557abe8728b7bfc8
Evanflow89 <ivanbaragone@gmail.com>
2022-09-21 11:40:44 +0200

    Add axios

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -7,6 +7,21 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      products: [],
+    };
+  },
+  created() {
+    axios
+      .get("http://127.0.0.1:8000/api/products")
+      .then((response) => {
+        this.products = response.data;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  },
 };
 </script>
 
